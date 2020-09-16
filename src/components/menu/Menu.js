7dacb2c7eb62be0ce8322e5e0b9bf217201772db
dfloresdev <hello@dflores.dev>
2020-09-16 03:06:54 -0500

    [FEATURE] menu ready

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.scss";
+import { FiSearch } from "react-icons/fi";
+import { BiMenuAltLeft } from "react-icons/bi";
+
+export const Menu = () => {
+  return (
+    <div className="containerMenu">
+      <BiMenuAltLeft className="styleIcon" />
+      <p>LATEST</p>
+      <p>FEATURED</p>
+      <p>AUTOMOBILES</p>
+      <p>CELEBRETIES</p>
+      <p>HOLLIWOOD DIRECTORY</p>
+      <p>TRENDING MOVIES</p>
+      <p>BOOK TICKET</p>
+      <p>FANS</p>
+      <p>BOOKS</p>
+      <FiSearch className="styleIcon" />
+    </div>
+  );
+};
--- src/layout/MainPage.js
+++ src/layout/MainPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../components/header/Header";
 import { ScrollList } from "../components/scrollList/ScrollList";
+import { Menu } from "../components/menu/Menu";
 
 export const MainPage = () => {
   return (
     <>
       <Header />
       <ScrollList />
+      <Menu />
     </>
   );
 };
