a45e66e06ccf6263d9625de65411ee7e7d9892cf
dfloresdev <hello@dflores.dev>
2020-09-16 01:25:04 -0500

    [FEATURE] header ready

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
+import deweys from "../../assets/deweys.png";
+import logo from "../../assets/logo.png";
+import phone from "../../assets/phone.png";
 import "./styles.scss";
 
 export const Header = () => {
   return (
-    <div>
-      <div>imagen 1</div>
-      <div>imagen 2</div>
-      <div>imagen 3</div>
+    <div className="containerHeader">
+      <div className="containerHeader--left">
+        <img src={deweys} />
+      </div>
+      <div className="containerHeader--center">
+        <img src={logo} />
+      </div>
+      <div className="containerHeader--right">
+        <img src={phone} />
+      </div>
     </div>
   );
 };
