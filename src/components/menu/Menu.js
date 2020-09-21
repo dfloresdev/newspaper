8ca2c40b8530d9e97d595cabb273f7a901286530
dfloresdev <hello@dflores.dev>
2020-09-21 15:08:27 -0500

    [RESPONSIVE] 100%

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,18 +5,24 @@ import { BiMenuAltLeft } from "react-icons/bi";
 
 export const Menu = () => {
   return (
-    <div className="containerMenu">
-      <BiMenuAltLeft className="styleIcon" />
-      <p>LATEST</p>
-      <p>FEATURED</p>
-      <p>AUTOMOBILES</p>
-      <p>CELEBRETIES</p>
-      <p>HOLLIWOOD DIRECTORY</p>
-      <p>TRENDING MOVIES</p>
-      <p>BOOK TICKET</p>
-      <p>FANS</p>
-      <p>BOOKS</p>
-      <FiSearch className="styleIcon" />
-    </div>
+    <>
+      <div className="containerMenu">
+        <BiMenuAltLeft className="styleIcon" />
+        <p>LATEST</p>
+        <p>FEATURED</p>
+        <p>AUTOMOBILES</p>
+        <p>CELEBRETIES</p>
+        <p>HOLLIWOOD DIRECTORY</p>
+        <p>TRENDING MOVIES</p>
+        <p>BOOK TICKET</p>
+        <p>FANS</p>
+        <p>BOOKS</p>
+        <FiSearch className="styleIcon" />
+      </div>
+      <div className="containerMenuResponsive">
+        <BiMenuAltLeft className="styleIcon" />
+        <FiSearch className="styleIcon" />
+      </div>
+    </>
   );
 };
